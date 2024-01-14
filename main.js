93aa4afd30a72a65289abeac49cea876928b3af4
efrice <719431749@qq.com>
2024-01-14 21:18:48 +0800

    feat: v2

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-// v1 pro
-const app = document.createElement("div")
-app.id = "app"
+// v2
+const textVNode = {
+  type: "TEXT_ELEMENT",
+  props: {
+    nodeValue: "app",
+    children: [],
+  },
+}
+
+const VNode = {
+  type: "div",
+  props: {
+    id: "app",
+    children: [textVNode],
+  },
+}
+
+const app = document.createElement(VNode.type)
+app.id = VNode.props.id
 document.getElementById("root").appendChild(app)
 
 const textEL = document.createTextNode("")
-textEL.nodeValue = "app"
+textEL.nodeValue = textVNode.props.nodeValue
 app.appendChild(textEL)
